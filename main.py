87c7ab68db0d4b662dd2f58afc7c5508287d9e8d
Noah Inocencio <ninocencio73@gmail.com>
2022-03-27 18:21:32 -0700

    Updated main and reports file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flat import Flatmate, Bill
 from reports import PDF
 
+
 def main():
     amount = float(input('What is the bill amount: '))
-    month = input('What is the bill period: Eg: December 2021 ')
-    the_bill = Bill(amount, month)
+    period = input('What is the bill period E.g. December 2021: ')
+    the_bill = Bill(amount, period)
     number_of_flatmates = int(input('How many flatmates are paying this period: '))
     flatmate_members = []
 
@@ -26,8 +27,8 @@ def main():
         this_flat_bill = flatmate_members[i].pays(the_bill.amount, days_sum)
         print(f"{flatmate_members[i].name} pays {this_flat_bill}")
 
-    # the_pdf = PDF('bill_report.pdf')
-    # the_pdf.generate(the_bill.amount, the_bill.month, flatmate_members)
+    the_pdf = PDF('bill_report.pdf')
+    the_pdf.generate(the_bill, flatmate_members, number_of_flatmates, days_sum)
 
 
 if __name__ == "__main__":
